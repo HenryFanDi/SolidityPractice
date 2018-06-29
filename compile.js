fc61c295570439f66263a4733b4fa7ab20959ced
Henry Fan <henry.fan@centraltrillion.com>
2018-06-29 15:41:32 +0800

    Section 2.33. Compiling Solidity

---
+++ compile.js
@@ -0,0 +1,15 @@
+// Path module will make sure that we always generate a valid path for other Unix based systems.
+const path = require('path');
+const fs = require('fs');
+
+// Solidity compiler
+const solc = require('solc');
+
+// Generating a path that points directly to the inbox
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+
+// Reading contents of file
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+// Compile will pass in our source code and then we have to specify the number of different contracts that we're attempting to compile
+console.log(solc.compile(source, 1));
